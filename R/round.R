19d02c9309d19595abefc9cd0f8dc23ba691dcb3
martinasladek <m.sladekova@brighton.ac.uk>
2022-06-17 15:49:03 +0100

    added initial documentation

--- R/round.R
+++ R/round.R
@@ -1,3 +1,14 @@
+#' Round: Round numbers. Without outrage.
+#'
+#' @param x a number. Just one. For now.
+#' @param digits integer indicating the number of decimal places. Negative values are probably allowed, but I haven't tested this, so maybe just... don't.
+#'
+#' @return a sensibly rounded number.
+#' @export
+#'
+#' @examples
+#' x <- 0.5
+#' round(x, digits = 0)
 round <- function(x, digits = 0){
 
   if(x == 0) return(0)
