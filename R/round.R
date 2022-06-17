84010a2a3340d4bce2251bdfea29ee5fb60fa6c6
martinasladek <m.sladekova@brighton.ac.uk>
2022-06-17 16:40:33 +0100

    updated dealing with negative digits

--- R/round.R
+++ R/round.R
@@ -1,19 +1,19 @@
 #' Round: Round numbers. Without outrage.
 #'
 #' @param x a numeric vector
-#' @param digits a positive integer indicating the number of decimal places.
+#' @param digits a positive integer indicating the number of decimal places. Negative integers will be converted to absolute values.
 #'
 #' @return a sensibly rounded number.
 #' @export
 #'
 #' @examples
-#' x <- 0.5
+#' x <- c(0.5, 1.5, 2.5, 3.5, 4.5)
 #' round(x, digits = 0)
 round <- function(x, digits = 0){
 
-  lil_bit <- 0.1^(digits + 1)
+  lil_bit <- 0.1^(abs(digits) + 1)
   sign = ifelse(x == 0, 1, x/abs(x))
 
-  base::round(x + sign*lil_bit, digits = digits)
+  base::round(x + sign*lil_bit, digits = abs(digits))
 
 }
