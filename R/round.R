e5e1c4e52e554b095580a9d0781a651bfe0d9238
martinasladek <m.sladekova@brighton.ac.uk>
2022-06-17 16:29:42 +0100

    updated to work with vectors

--- R/round.R
+++ R/round.R
@@ -1,7 +1,7 @@
 #' Round: Round numbers. Without outrage.
 #'
-#' @param x a number. Just one. For now.
-#' @param digits integer indicating the number of decimal places. Negative values are probably allowed, but I haven't tested this, so maybe just... don't.
+#' @param x a numeric vector
+#' @param digits a positive integer indicating the number of decimal places.
 #'
 #' @return a sensibly rounded number.
 #' @export
@@ -11,10 +11,8 @@
 #' round(x, digits = 0)
 round <- function(x, digits = 0){
 
-  if(x == 0) return(0)
-
   lil_bit <- 0.1^(digits + 1)
-  sign = x/abs(x)
+  sign = ifelse(x == 0, 1, x/abs(x))
 
   base::round(x + sign*lil_bit, digits = digits)
 
